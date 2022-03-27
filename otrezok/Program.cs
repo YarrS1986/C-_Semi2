2c9c1857157e33a888f5b2a07d607b0a4d850949
Yaroslav.S <sysoev.yar@yandex.ru>
2022-03-27 21:39:34 +0500

    отрезок из 2х чисел, большая цифра

---
+++ otrezok/Program.cs
@@ -0,0 +1,19 @@
+﻿//2. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//Например 87 -> 8
+Console.Clear();
+
+Random rnd = new Random();
+int value = rnd.Next(10, 99);
+
+Console.WriteLine($"Сгенерированное число -> {value}");
+
+int firstNum = value / 10;
+int secondNum = value % 10;
+int maxNum = 0;
+
+if (firstNum > secondNum)
+{
+    maxNum = secondNum;
+}
+maxNum = firstNum;
+Console.WriteLine($"Максимальная цифра -> {maxNum}");
