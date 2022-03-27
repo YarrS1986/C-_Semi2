9d3ea40ecead2678d104b2a7522e5a71eff8e15a
Yaroslav.S <sysoev.yar@yandex.ru>
2022-03-27 22:48:31 +0500

    остаток деления на 7 и 23

---
+++ delenie7+23/Program.cs
@@ -0,0 +1,13 @@
+﻿//4. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// напрмер 14 -> нет, 46 -> нет, 161 -> да
+
+Console.Clear();
+
+Console.Write("Введи число = ");
+string Number = Console.ReadLine();
+int num = int.Parse(Number);
+int result1 = num % 7;
+int result2 = num % 23;
+
+if (result1 == 0 && result2 == 0) Console.Write($"{num} -> да");
+else Console.Write($"{num} -> нет");
