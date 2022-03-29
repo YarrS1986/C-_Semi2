b44527eb67bbbbe668b24b5776a40f8a34acd7d5
Yaroslav.S <sysoev.yar@yandex.ru>
2022-03-29 21:12:23 +0500

    дз2 об удалении 2го числа в 3х значном числе

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// Пример, 456 -> 46, 782 -> 72, 918 -> 98
+
+Console.Clear();
+
+Random rnd = new Random();
+int Number=rnd.Next(100, 999);
+
+Console.WriteLine($"Сгенерированное число -> {Number}");
+
+int firstNum = Number / 100;
+int lastNum = Number % 10;
+
+Console.WriteLine($"Число без 2го символа -> {firstNum}{lastNum}");
