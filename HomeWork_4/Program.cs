932514696ca6bc515c705c9db0bfb0db8af8083f
Yaroslav.S <sysoev.yar@yandex.ru>
2022-03-29 22:30:11 +0500

    дз4 6,7 - выходной день?

---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Пример, 6 -> да, 7 -> да, 1 -> нет
+
+Console.Clear();
+
+Console.Write("Введи число от 1 до 7: ");
+string value = Console.ReadLine();
+int number = int.Parse(value);
+
+if (number > 7) Console.WriteLine("Введи правильную цифру!");
+else if (number < 1) Console.Write("Введи правильную цифру!");
+else if (number >= 1 & number <= 5) Console.Write($"{number} - будний день");
+else if (number >= 6 & number <= 7) Console.Write($"{number} - выходной день");
+
+// else if (number == 1) Console.Write("1 - Понедельник - будний день");
+// else if (number == 2) Console.Write("2 - Вторник - будний день");
+// else if (number == 3) Console.Write("3 - Среда - будний день");
+// else if (number == 4) Console.Write("4 - Четверг - будний день");
+// else if (number == 5) Console.Write("5 - Пятница - будний день");
+// else if (number == 6) Console.Write("6 - Суббота - выходной день");
+// else if (number == 7) Console.Write("7 - Воскресенье - выходной день");
